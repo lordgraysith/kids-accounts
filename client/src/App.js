bf051e480e9d0f69b9fb10f7910fd5c19c831541
Michael Graybeal <michael@thegraybeals.com>
2018-03-26 16:44:13 -0600

    react-md in the house!

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+import { NavigationDrawer } from 'react-md'
 import logo from './logo.svg'
 
 import './App.css'
@@ -26,13 +26,20 @@ class App extends Component {
 
   render () {
     return (
-      <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <h1 className='App-title'>Welcome to React</h1>
-        </header>
-        <p className='App-intro'>{this.state.response}</p>
-      </div>
+      <NavigationDrawer
+        drawerTitle='react-md with CRA'
+        toolbarTitle='Welcome to react-md'
+      >
+        <div className='App'>
+          <header className='App-header'>
+            <img src={logo} className='App-logo' alt='logo' />
+            <h1 className='App-title'>Welcome to React</h1>
+          </header>
+          <p className='App-intro'>
+            {this.state.response}
+          </p>
+        </div>
+      </NavigationDrawer>
     )
   }
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import WebFontLoader from 'webfontloader'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+WebFontLoader.load({
+  google: {
+    families: ['Roboto:300,400,500,700', 'Material Icons']
+  }
+})
+
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
